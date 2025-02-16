cf0405eb73282dd6eaf25b9cba4e85ba7571f21e
arnoldsschwarzenegger <karneltomujpies@outlook.com>
2025-02-16 09:19:30 +0100

    Create botv2.py
    updated bot.py

---
+++ botv2.py
@@ -0,0 +1,53 @@
+import discord
+import random
+from discord.ext import commands
+
+komendy = ["?hello", "?heh", "?roll", "?help"] 
+
+description = '''bot'''
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Zalogowaliśmy się jako {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    """Bot introduction"""
+    await ctx.send(f'Cześć, jestem bot{bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+@bot.command()
+async def help(ctx):
+    await ctx.send(komendy)
+
+async def on_message_edit(self, before, after):
+        msg = f'**{before.author}** edited their message:\n{before.content} -> {after.content}'
+        await before.channel.send(msg)
+
+bot.run("TOKEN")
