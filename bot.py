a8f63abfd5965b70d410be21c2c7a901bdc23f98
arnoldsschwarzenegger <karneltomujpies@outlook.com>
2025-02-23 12:09:53 +0100

    Update bot.py
    memes

--- bot.py
+++ bot.py
@@ -1,58 +1,32 @@
 import discord
 import random
+import os
 from discord.ext import commands
 
-description = '''An example bot to showcase the discord.ext.commands extension
-module.'''
-
 intents = discord.Intents.default()
-intents.members = True
 intents.message_content = True
-
-bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+bot = commands.Bot(command_prefix='?',  intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'Zalogowaliśmy się jako {bot.user}')
 
-@bot.command()
-async def hello(ctx):
-    await ctx.send(f'Cześć, jestem bot{bot.user}!')
 
 @bot.command()
-async def heh(ctx, count_heh = 5):
-    await ctx.send("he" * count_heh)
+async def mem_programowanie(ctx):
+    lista = os.listdir('programowanie')
+    img_name = random.choice(lista)
+    with open(f'programowanie/{img_name}', 'rb') as f:
+            picture = discord.File(f)
+    await ctx.send(file=picture)
 
 @bot.command()
-async def joined(ctx, member: discord.Member):
-    """Says when a member joined."""
-    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
-
-@bot.group()
-async def cool(ctx):
-    """Says if a user is cool.
-
-    In reality this just checks if a subcommand is being invoked.
-    """
-    if ctx.invoked_subcommand is None:
-        await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
+async def mem_zwierzeta(ctx):
+    lista2 = os.listdir('zwierzeta')
+    img_name = random.choice(lista2)
+    with open(f'zwierzeta/{img_name}', 'rb') as f:
+            picture = discord.File(f)
+    await ctx.send(file=picture)
 
 
-@cool.command(name='bot')
-async def _bot(ctx):
-    """Is the bot cool?"""
-    await ctx.send('Yes, the bot is cool.')
-
-@bot.command()
-async def roll(ctx, dice: str):
-    """Rolls a dice in NdN format."""
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await ctx.send('Format has to be in NdN!')
-        return
-
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await ctx.send(result)
-
-bot.run("token")
+bot.run("TOKEN")
